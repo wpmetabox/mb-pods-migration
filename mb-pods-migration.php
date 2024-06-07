f95311f54ac3856ba1542bf9852fd7f76cefdacd
LAPTOP-2R075DES\HP Pavilion <lexuanbao161096@gmail.com>
2024-06-07 14:58:29 +0700

    Update gitignore

--- mb-pods-migration.php
+++ mb-pods-migration.php
@@ -12,7 +12,19 @@
  */
 
 defined( 'ABSPATH' ) || die;
+/*	$query = new WP_Query( [ 
+			'post_type'              => '_pods_pod',
+			'post_status'            => 'any',
+			'posts_per_page'         => -1,
+			'no_found_rows'          => true,
+			'update_post_meta_cache' => false,
+			'update_post_term_cache' => false,
+		] );
 
+var_dump( $query->posts);die;*/
+/*$item = [ 'a' => get_post_meta( 408, 'built_in_taxonomies_danh-muc-san-pham', true ) ];
+$a = wp_json_encode( $item, JSON_UNESCAPED_UNICODE );
+var_dump( $a ); die;*/
 if ( ! function_exists( 'mb_pods_load' ) ) {
 	if ( file_exists( __DIR__ . '/vendor' ) ) {
 		require __DIR__ . '/vendor/autoload.php';
