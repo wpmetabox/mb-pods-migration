7df67feb3972a555aec15bb8b883171837e3ee36
LAPTOP-2R075DES\HP Pavilion <lexuanbao161096@gmail.com>
2024-06-15 16:25:15 +0700

    Update field pick

--- src/Processors/FieldGroups/FieldType.php
+++ src/Processors/FieldGroups/FieldType.php
@@ -83,11 +83,44 @@ class FieldType {
 		$this->type = 'text';
 	}
 
+	private function migrate_pick() {
+		$format_type =  get_post_meta( $this->post_id, 'pick_format_type', true );
+		$format = get_post_meta( $this->post_id, 'pick_format_single', true );
+		if ( $format_type == 'multi' ){
+			$format = get_post_meta( $this->post_id, 'pick_format_multi', true );
+			$this->multiple = true;
+		}
+
+		switch ( $format ) {
+			case 'dropdown':
+			$type = 'select';
+			break;
+			case 'radio':
+			$type = 'radio';
+			break;
+			case 'checkbox':
+			$type = 'checkbox_list';
+			break;
+			case 'multiselect':
+			$type = 'select';
+			break;
+			default:
+			$type = 'select_advanced';
+			break;
+		}
+
+		$this->type = $type;
+
+		$pick_custom = get_post_meta( $this->post_id, 'pick_custom', true);
+		$this->options = str_replace('|', ': ', $pick_custom );
+
+	}
+
 	private function migrate_file() {
-		$file_uploader = get_post_meta( $post_id, 'file_uploader', true );
-		$this->max_file_uploads = get_post_meta( $post_id, 'file_format_type', true ) == 'single' ? 1 : '';
+		$file_uploader = get_post_meta( $this->post_id, 'file_uploader', true );
+		$this->max_file_uploads = get_post_meta( $this->post_id, 'file_format_type', true ) == 'single' ? 1 : '';
 		$this->type =  ( $file_uploader == 'attachment' ) ? 'file_advanced' : 'file_upload';
-		$file_type = get_post_meta( $post_id, 'file_uploader', true );
+		$file_type = get_post_meta( $this->post_id, 'file_uploader', true );
 		$image = [ 'images', 'images-any'];
 		$video = [ 'video', 'video-any' ];
 		if ( in_array( $file_type, $image) ){
@@ -99,7 +132,7 @@ class FieldType {
 	}
 
 	private function migrate_boolean() {
-		$type = get_post_meta( $post_id, 'boolean_format_type', true ) ?: 'radio';
+		$type = get_post_meta( $this->post_id, 'boolean_format_type', true ) ?: 'radio';
 		$this->type = $type;
 		if( $type == 'checkbox' ){
 			$this->type = 'checkbox_list';
@@ -108,8 +141,8 @@ class FieldType {
 			$this->type = 'select';
 		}
 		$options = [ 
-			'1' => get_post_meta( $post_id, 'boolean_yes_label', true) ?: 'Yes',
-			'0' => get_post_meta( $post_id, 'boolean_no_label', true) ?: 'No',
+			'1' => get_post_meta( $this->post_id, 'boolean_yes_label', true) ?: 'Yes',
+			'0' => get_post_meta( $this->post_id, 'boolean_no_label', true) ?: 'No',
 		];
 		$values = [];
 		foreach( $options as $key => $value ){
@@ -121,10 +154,10 @@ class FieldType {
 
 	private function migrate_html() {
 		$this->type = 'custom_html';
-		$this->std  = get_post_meta( $post_id, 'html_content', true );
+		$this->std  = get_post_meta( $this->post_id, 'html_content', true );
 	}
 	
 	private function migrate_heading() {
-		$this->desc = get_post_meta( $post_id, 'html_tag', true );
+		$this->desc = get_post_meta( $this->post_id, 'html_tag', true );
 	}
 }
